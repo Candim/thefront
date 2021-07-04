19b03d7f7c6da3e865e6255bf80c0dfc59b835f8
Candim <83619064+Candim@users.noreply.github.com>
2021-07-04 14:27:45 -0300

    Criacao do componente de texto e botao

---
+++ src/components/ButtonPrimary/styles.js
@@ -0,0 +1,11 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const styles = makeStyles (() => ({
+    button: {
+        backgroundColor:"#3f51b5",
+        color:"white",
+    },
+}));
+
+export default styles;
+    
