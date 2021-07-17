b0daf2bbe8e240bb583106e5c923be4de79a9890
Candim <83619064+Candim@users.noreply.github.com>
2021-07-17 17:59:40 -0300

    criado componente de icone

---
+++ src/Home/Sectioninfo/index.js
@@ -0,0 +1,15 @@
+import  Grid  from '@material-ui/core/Grid';
+import  Container  from '@material-ui/core/Container';
+import React from 'react';
+
+const SectionInfo = () => {
+    return (
+        <Container maxwidth="lg">
+            <Grid container justify="center">
+                <Title text="Less code. More speed"
+
+            <Grid/>
+        <Container/>
+
+    )
+}
--- src/Home/Sectionintro/index.js
+++ src/Home/Sectionintro/index.js
@@ -1,15 +1,19 @@
 import React from "react";
-import styles from './styles';
-import {Title, Body, Subtitle} from '../../components/Text';
 import Grid from "@material-ui/core/Grid";
-
+import Container from "@material-ui/core/Container";
+import Icon from '../../components/Icon';
+import LayersIcon from '@material-ui/icons/Layers';
+import SignalWifi4BarIcon from '@material-ui/icons/SignalWifi4Bar';
+import CodeIcon from '@material-ui/icons/Code';
+import {Title, Body, Subtitle} from '../../components/Text';
+import styles from './styles';
 
 const SectionIntro = () => {
     const classes = styles();
     return(
-        <div>
-            <Grid container justify="center">
-                <Grid item md={3}>   
+        <Container maxWidth='lg'>
+            <Grid container justify="center" alignItems="center">
+                <Grid item md={6}>   
                 <Title 
                     text="Build accessible React apps with speed" 
                     style={{ textAling:"center" }}
@@ -23,25 +27,28 @@ const SectionIntro = () => {
 
             <Grid container justify="center" spacing={8}>
                 <Grid item md={3}>
-                <Subtitle text="Built for developers"/>
-                <Body text="TheFront is built to make your life easier. Variables, build tooling,
-                 documentation, and reusable components." 
-                 style={{color:'#718096'}} />
+                    <Icon component={<LayersIcon className={classes.icon}/>} />
+                    <Subtitle text="Built for developers"/>
+                    <Body text="TheFront is built to make your life easier. Variables, build tooling,
+                    documentation, and reusable components." 
+                    style={{color:'#718096'}} />
                 </Grid>
                 <Grid item md={3}>
+                <Icon component={<SignalWifi4BarIcon className={classes.icon}/>} />
                 <Subtitle text="Designed to be modern"/>
                 <Body text="Designed with the latest design trends in mind. TheFront feels modern,
                  minimal, and beautiful."
                 style={{color:'#718096'}} />
                 </Grid>
                 <Grid item md={3}>
+                <Icon component={<CodeIcon className={classes.icon}/>} />
                 <Subtitle text="Documentation for everything"/>
                 <Body text="We've written extensive documentation for components and tools,
                  so you never have to reverse engineer anything." 
                  style={{color:'#718096'}} />
                 </Grid>
             </Grid>
-        </div>
+        </Container>
 
       
     );
